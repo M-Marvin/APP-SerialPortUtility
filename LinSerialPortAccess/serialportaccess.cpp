c8c847099cb27225b2e5dc42e401f476f1e1d358
M_Marvin <marvin.koehler@inexio.email>
2024-04-05 23:24:17 +0200

    Combine linux and windows C++ in one Project (completed)

--- LinSerialPortAccess/serialportaccess.cpp
+++
@@ -1,3 +0,0 @@
-
-#include "serial_port.h"
-#include <de_m_marvin_serialportaccess_SerialPort.h>
---
+++ SerialPortAccess/serial_port.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifdef PLATFORM_WIN
+#define LIB_EXPORT __declspec(dllexport)
+#define LIB_IMPORT __declspec(dllimport)
+#else
+#define LIB_EXPORT
+#define LIB_IMPORT
+#endif
+
+struct SerialPortImplData;
+
+class SerialPort
+{
+
+public:
+	LIB_EXPORT SerialPort(const char* portFile);
+	LIB_EXPORT ~SerialPort();
+	LIB_EXPORT void setBaud(int baud);
+	LIB_EXPORT int getBaud();
+	LIB_EXPORT void setTimeouts(int readTimeout, int writeTimeout);
+	LIB_EXPORT bool openPort();
+	LIB_EXPORT void closePort();
+	LIB_EXPORT bool isOpen();
+	LIB_EXPORT unsigned long readBytes(char* buffer, unsigned long bufferCapacity);
+	LIB_EXPORT unsigned long readBytesConsecutive(char* buffer, unsigned long bufferCapacity, long long consecutiveDelay, long long receptionWaitTimeout);
+	LIB_EXPORT unsigned long writeBytes(const char* buffer, unsigned long bufferLength);
+
+private:
+	SerialPortImplData* implData;
+	
+};
+
